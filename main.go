80ce4c0c3a8526b75293913319bbc1531c65cc3f
isnastish <paukden12@gmail.com>
2024-12-09 12:12:58 +0100

    added gorrila/mux support

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"io"
 	"net/http"
 
 	"github.com/isnastish/nibble/pkg/log"
 )
 
-func helloRoute() {
-
+func helloRoute(respWriter http.ResponseWriter, req *http.Request) {
+	io.WriteString(respWriter, "Hello world!")
 }
 
 func main() {
+	http.HandleFunc("/hello", helloRoute)
+
 	log.Logger.Info("Listeing on port %s", ":3030")
 
 	if err := http.ListenAndServe(":3030", nil); err != nil {
