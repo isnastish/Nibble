ecb35c86a3a4b72c258e39eb363ac3c5f630b698
isnastish <paukden12@gmail.com>
2024-12-09 12:39:27 +0100

    sketched out ipresolver package

--- main.go
+++ main.go
@@ -9,6 +9,17 @@ import (
 	"github.com/isnastish/nibble/pkg/log"
 )
 
+type ApiServer struct {
+	*http.Server
+	// ip resolver client
+	// http server
+	// port, addr
+}
+
+func NewApiServer(port int) (*ApiServer, error) {
+	return &ApiServer{}, nil
+}
+
 func helloRoute(respWriter http.ResponseWriter, req *http.Request) {
 	io.WriteString(respWriter, "Hello world!")
 }
@@ -21,7 +32,7 @@ func main() {
 
 	log.Logger.Info("Listeing on port %s", ":3030")
 
-	if err := http.ListenAndServe(":3030", nil); err != nil {
+	if err := http.ListenAndServe(":3030", nil); err != http.ErrServerClosed {
 		log.Logger.Fatal("Failed to server on port: %s", ":3030")
 	}
 }
---
+++ pkg/ipresolover/ipresolver.go
@@ -0,0 +1,12 @@
+package ipresolover
+
+type Client struct {
+}
+
+func NewClient() (*Client, error) {
+	// get API key from environment variable,
+	// if failed, return an error
+	// then the client should be assigned to API server
+	// for making queries to external service
+	return &Client{}, nil
+}
