fe021f2cb78e40de9342d69d8f14d50488687112
isnastish <paukden12@gmail.com>
2024-12-09 15:44:47 +0100

    get ipflare ip key from env

--- pkg/ipresolver/ipresolver.go
+++ pkg/ipresolver/ipresolver.go
@@ -1,13 +1,24 @@
 package ipresolver
 
+import (
+	"fmt"
+	"os"
+)
+
 type Client struct {
 }
 
 func NewClient() (*Client, error) {
+	IPFLARE_API_KEY, set := os.LookupEnv("IPFLARE_API_KEY")
+	if !set || IPFLARE_API_KEY == "" {
+		return nil, fmt.Errorf("IPFLARE_API_KEY is not set")
+	}
+
 	// get API key from environment variable,
 	// if failed, return an error
 	// then the client should be assigned to API server
 	// for making queries to external service
+
 	return &Client{}, nil
 }
 
@@ -17,3 +28,5 @@ func NewClient() (*Client, error) {
 func (c *Client) Resolve(ipAddr string) (string, error) {
 	return "unknown", nil
 }
+
+// IPFLARE_API_KEY='d4815a7185da6aae.69f941c643a3f41f751fcc9ef59dcfcfed08a00fb57907b4e750a4a1cdbffc3a'
