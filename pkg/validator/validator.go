1cd07ac7c2b24bb9f447eb3bdc92b7191297f3f8
isnastish <paukden12@gmail.com>
2024-12-10 15:33:05 +0100

    added email address validation

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -2,17 +2,23 @@ package validator
 
 import (
 	"regexp"
+	"strings"
 )
 
-// Validate user's password. The password should contain at least 12,
+// Validate user's password. The password should contain at least 10,
 // and at most 32 characters. It's a simplified version of how
 // password validation could be achieved.
 // Returns true if passowrd is valid, false otherwise.
 func ValidateUserPassword(pwd string) bool {
-	re := regexp.MustCompile(`^[a-zA-Z0-9_@$#:&%]{12,32}$`)
-	return re.MatchString(pwd)
+	re := regexp.MustCompile(`^[a-zA-Z0-9_@$#:&%]`)
+	return len(pwd) >= 10 && len(pwd) <= 32 && re.MatchString(pwd)
 }
 
+// Validate user's email address. This is a bare minimum validation
+// which should never be used in a production, but is sufficient for our purposes.
+// Only checks if provided strings contains `@` symbol,
+// if so, returns true, false otherwise.
+// Since writing a complete email validator is not part of this assignment.
 func ValidateUserEmailAddress(email string) bool {
-	return false
+	return len(email) >= 3 && len(email) <= 320 && strings.Contains(email, "@")
 }
--- pkg/validator/validator_test.go
+++ pkg/validator/validator_test.go
@@ -14,4 +14,36 @@ func TestPasswordValidation(t *testing.T) {
 	if ValidateUserPassword(pwd) { // too short
 		t.Errorf("failed to validate password %s", pwd)
 	}
+
+	pwd = "!!!!!not allowed"
+	if ValidateUserPassword(pwd) { // contains not allowed symbols
+		t.Errorf("failed to validate password %s", pwd)
+	}
+
+	pwd = "isnastish@1234"
+	if !ValidateUserPassword(pwd) {
+		t.Errorf("password validation should have passed %s", pwd)
+	}
+
+	pwd = "too long of a password, so that the validation should fail"
+	if ValidateUserPassword(pwd) {
+		t.Errorf("failed to validation the password %s", pwd)
+	}
+}
+
+func TestEmailValidation(t *testing.T) {
+	email := ""
+	if ValidateUserEmailAddress(email) {
+		t.Errorf("failed to validate email address %s", email)
+	}
+
+	email = "@"
+	if ValidateUserEmailAddress(email) { // doesn't contain domain name
+		t.Errorf("failed to validate email address %s", email)
+	}
+
+	email = "isnastish@gmail.com"
+	if !ValidateUserEmailAddress(email) {
+		t.Errorf("email validation should have passed %s", email)
+	}
 }
