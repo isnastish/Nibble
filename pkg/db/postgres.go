6b600ead3119650c2656e5aa1e0ca8b552f75390
isnastish <paukden12@gmail.com>
2024-12-10 12:17:44 +0100

    connected postgres

--- pkg/db/postgres.go
+++ pkg/db/postgres.go
@@ -1,23 +1,52 @@
 package db
 
-// TODO: Use interface for databese so we can switch between
-// multiple databases, not only postgres
+import (
+	"context"
+	"fmt"
+	"os"
 
-// TODO: Use pgx library
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type PostgresDB struct {
+	connPool *pgxpool.Pool
 }
 
 func NewPostgresDB() (*PostgresDB, error) {
-	postgres := &PostgresDB{}
-	return postgres, nil
+	postgresUrl, set := os.LookupEnv("POSTGRES_URL")
+	if !set || postgresUrl == "" {
+		return nil, fmt.Errorf("postgres: POSTGRES_URL is not set")
+	}
+
+	config, err := pgxpool.ParseConfig(postgresUrl)
+	if err != nil {
+		return nil, fmt.Errorf("postgres: failed to parse config, error: %s", err.Error())
+	}
+
+	connPool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		return nil, fmt.Errorf("postgres: failed to create connection pool, error: %s", err.Error())
+	}
+
+	postgres := PostgresDB{
+		connPool: connPool,
+	}
+
+	if err := postgres.createTables(); err != nil {
+		return nil, err
+	}
+
+	return &postgres, nil
 }
 
 // TODO: Create tables for storing user data
-func (db *PostgresDB) createTables() {
+func (db *PostgresDB) createTables() error {
+	return nil
 }
 
 // TODO: Close database connection
 func (db *PostgresDB) Close() error {
+	defer db.connPool.Close()
+
 	return nil
 }
